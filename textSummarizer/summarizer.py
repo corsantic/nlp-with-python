26a8c9a2744fb41b1d8dcd7018d43d74d3153831
corsantic <aydofurkan25@gmail.com>
2018-12-11 13:03:56 +0300

    preprocessing the data

--- textSummarizer/summarizer.py
+++ textSummarizer/summarizer.py
@@ -1,7 +1,22 @@
 
 import bs4 as bs
 import urllib.request
+import re
 
 source = urllib.request.urlopen('https://www.bbc.com/news/technology-46516662')
 
 soup  = bs.BeautifulSoup(source, 'lxml')
+
+text = ""
+for paragraph in soup.find_all('p'):
+    text += paragraph.text
+
+
+# Preprocessing the text
+text = re.sub(r'Share this withEmailFacebookMessengerMessengerTwitterPinterestWhatsAppLinkedInCopy this linkThese are external links and will open in a new window',' ',text)
+text = re.sub(r'\s+',' ',text)
+clean_text = text.lower()
+clean_text = re.sub(r'\W',' ',clean_text)
+clean_text = re.sub(r'\d',' ',clean_text)
+clean_text = re.sub(r'\s+',' ',clean_text)
+
